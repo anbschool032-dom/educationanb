bfc5eff05d992ff6cb9bed8225675992acd1df61
anbschool032-dom <anbschool032@gmail.com>
2025-12-23 00:17:14 -0800

    gitionre env

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  optimizeDeps: {
-    include: ['xlsx'],
-  },
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        // If backend requires cookie forwarding:
-        // configure to preserve cookie domain/path if needed
+export default defineConfig(({ mode }) => {
+  // ផ្ទុក .env ដើម្បីយកមកប្រើក្នុងនេះបាន
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    optimizeDeps: {
+      include: ['xlsx'],
+    },
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          // ប្រើ Variable ពី .env ឬបើអត់មានប្រើ localhost:3000
+          target: env.VITE_API_URL || 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
