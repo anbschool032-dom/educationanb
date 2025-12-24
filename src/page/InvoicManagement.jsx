b12ac1e979dc0b09ed86a3e84c6ec823b5fcfc0f
anbschool032-dom <anbschool032@gmail.com>
2025-12-24 01:15:18 -0800

    test git ci/cd

--- src/page/InvoicManagement.jsx
+++ src/page/InvoicManagement.jsx
@@ -32,7 +32,6 @@ const InvoiceManagement = () => {
 
       {/* Header Text */}
       <h2 className="mb-4 text-dark fw-bold">Invoice Management</h2>
-      <h3>Hello pro ci/cd please work 🤨</h3>
       {/* Stats Cards - ដូរទៅពណ៌ស (bg-white) */}
       <div className="row mb-4">
         <div className="col-md-6 mb-3">
